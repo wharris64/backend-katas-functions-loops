977dea09efa7160f935744fd35b7446c3190ffaf
WILL_HARRIS <wharris64@ivytech.edu>
2019-09-04 14:06:18 -0400

    refactor

--- main.py
+++ main.py
@@ -1,19 +1,57 @@
 def add(x,y):
-  # your code here
-  return
+    n3 = 0
+    n3 = x + y
+    return n3
+add(2, 4)
 
 def multiply(x,y):
-  # your code here
-  return
+  var1 = 0
+  for i in range (1, y + 1):
+    var1 = add(var1, x)
+  return var1
+multiply(6,8)
+  
 
 def power(x,n):
-  # your code here
-  return
+  prod = x
+  for i in range(1, n):
+    prod = multiply(prod, x)
+  return prod
+
+power(2, 8)
+
 
 def factorial(x):
-  # your code here
-  return
+  sum = 1
+  for i in range(1, x + 1):
+    sum = multiply(i, sum)  
+  return sum      
+
+factorial(4)
+
 
 def fibonacci(n):
-  # your code here
-  return
+        a = 0
+        b = 1
+        if n < 0:
+                result = "whaaaaat"
+        elif n == 0:
+                a = 0
+                result = 0
+        elif n == 1:
+                b = 1
+                result = 1
+        else:
+                for i in range(2,n):
+                        c = add(a,b)
+                        a = b
+                        b = c
+        return c
+
+
+
+print(add(2,4))
+print(multiply(6,8))
+print(power(2, 8))
+print(factorial(4))
+print(fibonacci(8))
